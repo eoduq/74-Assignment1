49bbc87296a71a92bf0f92ec3e763877eb8554d8
eoduq <98501081+eoduq@users.noreply.github.com>
2022-09-25 17:49:26 +0900

    Update handlError.cpp

--- Assignment1/handlError.cpp
+++ Assignment1/handlError.cpp
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include "handlError.h"
 #include "stdafx.h"
+//print last error code and message
 void handlError(void) {
-	DWORD error=GetLastError();
-	TCHAR errMessage[1024];
+	DWORD error=GetLastError();//variabe to store error code value
+	TCHAR errMessage[1024];//array to store error message
 	FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, NULL, error, 0, errMessage, 1024, NULL);
 	wprintf(L"Error_Code: %d, Error_Message: %s", error, errMessage);
-	exit(EXIT_FAILURE);
+	exit(EXIT_FAILURE);//unsuccessful execution of a program
 }
